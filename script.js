b9c876f613a40386d9adb74e79e3da679417055d
DRegan-dev <damienreganmusic@gmail.com>
2024-02-22 17:08:14 +0000

    fixed issue with .popup-menu not displaying

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const popupMenu = document.querySelector('.popup-menu');
 const main = document.querySelector('.main');
 
-menuBtn.onclick = () => {
+menuBtn.addEventListener('click', () => {
     popupMenu.classList.add('active');
     main.classList.add('active');
-}
+});
