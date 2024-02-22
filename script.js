e4ca6b824d40c91b18351366c59b0e7eb0e020a4
DRegan-dev <damienreganmusic@gmail.com>
2024-02-22 14:50:52 +0000

    added arrow function to handle the click event for menuBtn that adds active class to popupMenu and main

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const menuBtn = document.querySelector('.menu-btn');
+const popupMenu = document.querySelector('.popup-menu');
+const main = document.querySelector('.main');
+
+menuBtn.onclick = () => {
+    popupMenu.classList.add('active');
+    main.classList.add('active');
+}
