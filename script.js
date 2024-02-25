876dc616a3a8d57f27142f216302423d6667f1fc
DRegan-dev <damienreganmusic@gmail.com>
2024-02-25 16:53:53 +0000

    linked booking.html to book a table a tag on index html and added form to booking.html

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const menuBtn = document.querySelector('.menu-btn');
 const popupMenu = document.querySelector('.popup-menu');
 const main = document.querySelector('.main');
+const returnBtn = document.querySelector('.return-btn');
 
 menuBtn.addEventListener('click', () => {
     popupMenu.classList.add('active');
     main.classList.add('active');
+});
+
+returnBtn.addEventListener('click', () => {
+    popupMenu.classList.remove('active');
+    main.classList.remove('active');
 });
